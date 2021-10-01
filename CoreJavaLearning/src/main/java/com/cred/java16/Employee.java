fb4ad6881276e70375a7215b3bac523e74b76669
kumar <kumar@Abhay>
2021-10-01 17:23:56 +0530

    First Commit to GIT and GITHUB

---
+++ CoreJavaLearning/src/main/java/com/cred/java16/Employee.java
@@ -0,0 +1,5 @@
+package com.cred.java16;
+
+public record Employee(String empName, Integer empId, double salary) {
+	
+}
---
+++ CoreJavaLearning/src/main/java/com/cred/java16/RecordDemo.java
@@ -0,0 +1,12 @@
+package com.cred.java16;
+
+public class RecordDemo {
+
+	public static void main(String[] args) {
+
+		Employee emp1=new Employee("Abhay", 2124, 115000.00);
+		System.out.println(emp1);
+		System.out.println(emp1.empName());
+	}
+
+}
